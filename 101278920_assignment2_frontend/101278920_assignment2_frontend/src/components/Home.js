4e436a3f9237742d33d2105b30577eae924c8c1e
Kanav306 <56760307+Kanav306@users.noreply.github.com>
2021-12-05 23:58:48 -0500

    Add files via upload

---
+++ 101278920_assignment2_frontend/101278920_assignment2_frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Hello! COMP 3123</h1>
+                </div>
+            </div>
+        )
+    }
+}
